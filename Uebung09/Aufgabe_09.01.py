3f4653ae59da986f1e1f567a411f8df8b9a68e9f
StefanSidler95 <stefan.sidler@students.fhnw.ch>
2022-12-13 15:44:01 +0100

    Uebung09
    Korrektur

--- Uebung09/Aufgabe_09.01.py
+++ Uebung09/Aufgabe_09.01.py
@@ -1,3 +1,5 @@
+#lokal läuft es, aber nicht geschafft, auf den Server zu laden...
+
 from pyproj import Transformer
 import uvicorn
 from fastapi import FastAPI
@@ -10,12 +12,21 @@ app = FastAPI()
 @app.get("/transform/wgs84lv95")
 async def transform(lat: float, lng: float):
     r1 = transformer1.transform(lng, lat)
-    return f"""east: {r1[0]}; north: {r1[1]}"""
+    return f"""E: {r1[0]} | N: {r1[1]}"""
 
 @app.get("/transform/lv95wgs84")
 async def transform(east: float, north: float):
     r2 = transformer2.transform(east, north)
-    return f"""lng: {r2[0]}; lat: {r2[1]}"""
+    return f"""lat: {r2[0]} | lon: {r2[1]}"""
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8001, root_path="/transform")
+
+#Eingabe-Beispiel für Luzern (Lokal):
+#http://127.0.0.1:8001/transform/wgs84lv95?lng=47.05125&lat=8.30730
+#http://127.0.0.1:8001/transform/lv95wgs84?east=2666003&north=1211500
+
+#Eingabe-Beispiel für Luzern (Server):
+#https://vm29.sourcelab.ch/transform/wgs84lv95?lng=47.05125&lat=8.30730
+#https://vm29.sourcelab.ch/transform/lv95wgs84?east=2666003&north=1211500
+
--- Uebung09/Aufgabe_09.02.py
+++ Uebung09/Aufgabe_09.02.py
@@ -1,18 +1,28 @@
+#lokal läuft es, aber nicht geschafft, auf den Server zu laden...
+
 import pyproj
 import uvicorn
 from fastapi import FastAPI, Response
+
 g = pyproj.Geod(ellps="WGS84")
 
 app = FastAPI()
 
 @app.get("/geodetic/&")
-async def line(slat: float, slng: float, elat: float, elng: float):
-    lonlats = g.npts(slng, slat, elng, elat, 30)
-    lonlats = [[slng, slat]] + [list(i) for i in lonlats] + [[elng, elat]] ## add start and end point
+async def line(slat: float, slng: float, elat: float, elng: float): # Eingabe von Start und Endpunkt
+    lonlats = g.npts(slng, slat, elng, elat, 50)
+    lonlats = [[slng, slat]] + [list(i) for i in lonlats] + [[elng, elat]] 
     lonlats = str(lonlats)
-    s = '"'
-    geo = '{"type" : "Feature", "geometry": {"type": "LineString","coordinates": ' + lonlats +'}, "properties": {"about": "Geodätische Linie"}}'
+    #s = '"'
+    geo = '{"type" : "Feature", "geometry": {"type": "LineString","coordinates": ' + lonlats +'}, "properties": {"about": "Geodetic Line"}}'
     return Response(content=geo)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8002, root_path="/geodetic")
+
+#Eingabebeispiel (Oslo - Mexico City):
+#http://127.0.0.1:8002/geodetic/&?slat=59.89&slng=10.62&elat=19.43&elng=-99.07
+#Geodätische Linie anschauen unter: https://geojson.tools/
+
+#Eingabebeispiel (auf Server):
+#https://vm29.sourcelab.ch/geodetic/&?slat=59.89&slng=10.62&elat=19.43&elng=-99.07
