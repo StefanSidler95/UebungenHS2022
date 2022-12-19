df2f883a7b7d32a52be12f6e211fadf1b7fc1002
StefanSidler95 <stefan.sidler@students.fhnw.ch>
2022-12-19 17:54:23 +0100

    Gruppenprojekt
    Version 1.0

---
+++ Projekt/projekt/src/App.js
@@ -0,0 +1,233 @@
+// Alex Bura, Dominic Schär, Stefan Sidler / FHNW Institut Geomatik / Version 1.0 / 19.12.2022
+
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from 'react-leaflet'
+import Grid from '@mui/material/Grid';
+import Textfield from '@mui/material/Textfield';
+import axios from "axios";
+import { Typography } from '@mui/material';
+import Dropdown from 'react-dropdown';
+import L from 'leaflet';
+import Slider from '@mui/material/Slider';
+import Toolbar from "@mui/material/Toolbar";
+import AppBar from "@mui/material/AppBar";
+import 'react-dropdown/style.css';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-geometryutil';
+import "./App.css";
+import { color } from '@mui/system';
+
+const yellowMarker = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  
+  const [start_lat, setStart_lat] = useState(0);
+  const [start_lng, setStart_lng] = useState(0);
+  const [end_lat, setEnd_lat] = useState(0);
+  const [end_lng, setEnd_lng] = useState(0);
+  const [tempo, setTempo] = useState(1000);
+  const points_num = 100
+
+  // eine Liste mit 20 Beispielflughäfen aus aller Welt
+  const airports = [  
+      ["Choose Airport", 0, 0],
+      ["Zürich Airport (Switzerland)", 47.46, 8.54],
+      ["London Heathrow Airport (United Kingdom)", 51.47, -0.46],
+      ["Adolfo Suárez Madrid–Barajas Airport (Spain)", 40.47, -3.56],
+      ["Gran Canaria Airport", 27.93, -15.38],
+      ["Tromsø Airport (Norway)", 69.68, 18.91],
+      ["Helsinki Vantaa Airport (Finland)", 60.31, 24.96],
+      ["Istanbul Airport (Turkey)", 41.26, 28.74],
+      ["Cairo International Airport (Egypt)", 30.12, 31.41],
+      ["Cape Town International Airport (South Africa)", -33.96, 18.61],
+      ["Sydney Kingsford Smith International Airport (Australia)", -33.94, 151.17],
+      ["Auckland International Airport (New Zealand)", -37.01, 174.79],
+      ["Miami International Airport (USA)", 25.79, -80.29],
+      ["Los Angeles International Airport (USA)", 33.94, -118.41],
+      ["Fairbanks International Airport (Alaska)", 64.81, -147.85],
+      ["Tocumen International Airport (Panama)", 9.07, -79.38],
+      ["Congonhas Airport (Brazil)", -23.62, -46.65],
+      ["Tokyo Haneda International Airport (Japan)", 35.55, 139.77],
+      ["Shanghai Pudong International Airport (China)", 31.14, 121.81],
+      ["Singapore Changi Airport", 1.35019, 103.994003],
+      ["Lukla Airport (Nepal)", 27.68, 86.72]
+    ];
+
+  // Optionen für die DropDown-Menüs  
+  const options = airports.map((airport) => airport[0]);
+
+  // Fragt die Koordinaten des Starpunktes ab, gemäss Auswahl im DropDown-Menü
+  function change_start(option) {
+    const selectedAirport = airports.find(airport => airport[0] === option.value);
+    setStart_lat(selectedAirport[1]);
+    setStart_lng(selectedAirport[2]);
+  }
+
+  // Fragt die Koordinaten des Endpunktes ab, gemäss Auswahl im DropDown-Menü
+  function change_end(option) {
+    const selectedAirport = airports.find(airport => airport[0] === option.value);
+    setEnd_lat(selectedAirport[1]);
+    setEnd_lng(selectedAirport[2]);
+
+  }
+  
+  // Seite neu laden, für Reset oder Neue Berechung  
+  function reload() {
+    window.location.reload();
+  }
+  
+  // Berechnung der geodätischen Linie von Externen Server
+  function do_download() {
+    var url = `https://vm1.sourcelab.ch/geodetic/line?startlat=${start_lat}&startlng=${start_lng}&endlat=${end_lat}&endlng=${end_lng}&pts=${points_num}`
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  // Berechnet die Länge zwischen den beiden Punkten
+  const line = L.polyline([
+    L.latLng(start_lat, start_lng),
+    L.latLng(end_lat, end_lng)
+  ]);
+  
+  // Anhand der Länge und einer Fluggeschwindigkeit wird die ungefähre Flugzeit ausgegeben.
+  const distance = L.GeometryUtil.length(line);
+  const distance_km = Math.round(distance / 1000);
+  const time = Math.round(distance_km / tempo);
+
+  useEffect(() => {
+    const L = require("leaflet");
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+    });
+    }, []);
+
+  return (
+    <>   
+      <AppBar position='sticky' className="appbar">
+      <Toolbar>
+        <Grid container justifyContent="flex-start">
+          <Typography>Geodätische Linie zwischen Flughäfen</Typography>
+        </Grid>
+        <Grid container justifyContent="flex-end">
+          <Button href="https://www.fhnw.ch/de/studium/architektur-bau-geomatik/bachelor-studiengang-geomatik" color="inherit">Über die FHNW</Button>
+          <Button href="https://leafletjs.com/" color="inherit">Über Leaflet</Button>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+      
+      {!data && <>
+          <Grid container spacing={2}>
+            <Grid item xs = {12}>
+              <h3><br/>Wähle zwei Flughäfen aus der Liste aus, um eine geodätische Linie und die ungefähre Flugzeit zu berechnen.</h3>
+              <Grid item xs = {6}>
+              <p>Startpunkt (blau)</p>              
+              <Dropdown options={options} onChange={change_start} value={airports[0][0]} />          
+              <p>Endpunkt (gelb)</p>
+              <Dropdown options={options} onChange={change_end} value={airports[0][0]} />
+              </Grid>
+            </Grid>
+            <Grid item xs = {12}>
+              <p>Alternativ können die Koordinaten unten von Hand eingegeben werden.</p>
+            </Grid>
+            <Grid item xs = {6}>
+              <Textfield fullWidth label="Start Lat" variant="outlined" input type="number" InputProps={{ inputProps: { min: "-89", max: "89", step: "1" } }} defaultValue={start_lat} onChange={ (event) => (setStart_lat(event.target.value)) }/> 
+            </Grid>
+            <Grid item xs = {6}>
+              <Textfield fullWidth label="Start Lng" variant="outlined" input type="number" InputProps={{ inputProps: { min: "-179", max: "179", step: "1" } }} defaultValue={start_lng} onChange={ (event) => (setStart_lng(event.target.value)) }/> 
+            </Grid>
+            <Grid item xs = {6}>
+              <Textfield fullWidth label="End Lat" variant="outlined" input type="number" InputProps={{ inputProps: { min: "-89", max: "89", step: "1" } }} defaultValue={end_lat} onChange={ (event) => (setEnd_lat(event.target.value)) }/> 
+            </Grid>
+            <Grid item xs = {6}>
+              <Textfield fullWidth label="End Lng" variant="outlined" input type="number" InputProps={{ inputProps: { min: "-179", max: "179", step: "1" } }} defaultValue={end_lng} onChange={ (event) => (setEnd_lng(event.target.value)) }/> 
+            </Grid>
+            <Grid item xs = {12}>
+            <MapContainer center={[24, 0]} zoom={2} scrollWheelZoom={true}
+                    style={{ height: "500px", width: "100%"}} >
+                  <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' minZoom={2} />
+                  <Marker position={[start_lat, start_lng]}/>
+                  <Marker position={[end_lat, end_lng]} icon={yellowMarker}/>
+                  </MapContainer>
+            </Grid>
+            <Grid item xs = {12}>
+            <p>Mit "Berechnung starten" wird folgende geodätische Linie berechnet: <br/>
+              <br/>Startpunk {start_lat} / {start_lng}  
+              <br/>Endpunkt {end_lat} / {end_lng} 
+            </p>
+            </Grid>
+            <Grid item xs = {12} >
+              <Button variant="contained" onClick={() => { do_download() }}>Berechnung starten</Button>
+              <Button variant="contained" onClick={() => { reload() }}>Reset</Button>
+            </Grid>
+            <Grid item xs = {12} >
+            <h4>Erstellt von Alex Bura, Dominic Schär und Stefan Sidler / FHNW Institut Geomatik / Version 1.0 / 19.12.2022</h4>
+            </Grid>
+          </Grid>
+          
+         </>}
+
+      {loading && <>
+                     <p>API Aufruf, bitte warten!</p><br/>
+                  </>}
+
+      {error &&   <>
+                     <p>ERROR API Aufruf fehlgeschlagen</p>{console.log(error)}<br/>
+                  </>}
+
+      {data &&  <>      
+        <MapContainer center={[24, 0]} zoom={2} scrollWheelZoom={true}
+                    style={{ height: "500px", width: "100%"}} >
+                  <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' minZoom={2} />
+                  <Marker position={[start_lat, start_lng]}><Popup>Startpunkt <br/>{start_lat}, {start_lng}</Popup></Marker>
+                  <Marker position={[end_lat, end_lng]} icon={yellowMarker}><Popup>Endpunkt <br/>{end_lat}, {end_lng}</Popup></Marker>
+                  <GeoJSON data={data} style={{ weight: 8, opacity: '50%', color: 'blue'}}/>
+                  </MapContainer>
+
+                  <Grid container spacing={2}> 
+                     <Grid item xs = {12}>
+                      <h3>Anschliessend kann für die beiden Destination die ungefähre Flugzeit berechnet werden</h3>
+                      </Grid>
+                      <Grid item xs = {6}>
+                      <p>Durchschnittliche Fluggeschwindigkeit (km/h) wälen</p>
+                      <Slider defaultValue={1000} max={2000} min={10} aria-label="Default" valueLabelDisplay="auto" onChange={ (event) => (setTempo(event.target.value)) }/>
+                      </Grid>
+                    <Grid item xs = {12}>
+                      <h3>Die Distanz beträgt {distance_km} km. <br/> Die Fluggzeit mit einer Fluggeschwindigkeit von {tempo} km/h beträgt etwa {time} Stunde(n).</h3>
+                    </Grid>
+                    <Grid item xs = {12}>
+                      <Button variant="contained" onClick={() => { reload() }}>Neue Berechnung durchführen</Button>
+                    </Grid>
+                    <Grid item xs = {12} >
+                    <h4>Erstellt von Alex Bura, Dominic Schär und Stefan Sidler / FHNW Institut Geomatik / Version 1.0 / 19.12.2022</h4>
+                    </Grid>
+                  </Grid>
+                </>}
+      </>);
+}
+export default App;
